0d8fb1afc1a11f9728a25b66018a93f31d4d27c3
Izzy <beta5553@gmail.com>
2019-04-16 16:31:15 -0600

    DjrController update

--- src/main/java/com/qbtrance/djrank/controller/djrController.java
+++ src/main/java/com/qbtrance/djrank/controller/djrController.java
@@ -1,17 +1,40 @@
 package com.qbtrance.djrank.controller;
 
+import com.qbtrance.djrank.dao.User;
+import com.qbtrance.djrank.dao.UserRepository;
+import com.qbtrance.djrank.service.WelcomeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class djrController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    WelcomeService welcomeService;
 
     // Redirect to index.html
     @GetMapping("/")
     public void djrStart(HttpServletResponse response) throws IOException {
         response.sendRedirect("/index.html");
     }
+
+    @GetMapping("/getUsers")
+    public List<User> getDjs()  {
+        return (List<User>) userRepository.findByUserId(43);
+    }
+
+
 }
